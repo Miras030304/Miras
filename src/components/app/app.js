309acc507b926d33bb907b916119657ae4fef3c6
Miras030304 <chilanov@inbox.ru>
2024-10-26 23:20:13 +0500

    step 28

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,7 @@ import ItemStatusFilter from '../item-status-filter/item-status-filter';
 import ItemAddForm from "../item-add-form";
 import './app.css';
 export  default  class  App extends Component {
+    maxId = 100;
     state = {
         todoData : [
                { label: 'Drink Coffee', important: false, id: 1 },
@@ -26,6 +27,25 @@ export  default  class  App extends Component {
             };
         });
     };
+    addItem = (text) => {
+        // console.log('Added', text);
+        // generate id  &  add element in array
+        const newItem = {
+            label: text,
+            important: false,
+            id: this.maxId++
+        };
+        this.setState(({todoData}) => {
+            // todoDate.push(newItem);
+            const newArr = [
+                ...todoData,
+                newItem
+            ];
+            return {
+                todoData: newArr
+            };
+        });
+    }
     render() {
         return (
             <div className="todo-app">
@@ -36,7 +56,7 @@ export  default  class  App extends Component {
                 </div>
                 <TodoList todos={ this.state.todoData}
                           onDeleted = { this.deleteItem }/>
-                <ItemAddForm />
+                 <ItemAddForm onItemAdded = {this.addItem } />
             </div>
         );
     };
