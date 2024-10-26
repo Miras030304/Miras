6dbb734fc4d649f78a37ba759b2da9f637d20e32
Miras030304 <chilanov@inbox.ru>
2024-10-26 20:05:57 +0500

    step 11

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -3,7 +3,7 @@ import TodoListItem from "./todo-list-item";
 const TodoList = ({ todos}) => {
     const elements = todos.map((item) => {
         return (
-            <li key = {item.id} >
+            <li key = { item.id } >
             <TodoListItem {...itemProps } />
         </li>
         );
